6992599991df1a69991dfbd8279665d9a99cc2e3
fullstackdev7731 <113845586+fullstackdev7731@users.noreply.github.com>
2022-10-26 20:04:20 +0300

    Update urls.py

--- visitors/urls.py
+++ visitors/urls.py
@@ -5,7 +5,7 @@ from .views import (visitor_homepage,
                     ReservationList,
                     ReservationView)
 urlpatterns = [
-    path('', visitor_homepage, name='visitor_homepage'),
+    path('homepage', visitor_homepage, name='visitor_homepage'),
     path('create_reservation', create_reservation, name='create_reservation'),
     path('reservations', ReservationList.as_view(), name='reservations'),
     path('reservation-detail/<int:pk>', ReservationView.as_view(), name='reservation_detail'),
