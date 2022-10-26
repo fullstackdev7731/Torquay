b2cfea5c77f8ea10632891ad72344eb859e61f02
fullstackdev7731 <113845586+fullstackdev7731@users.noreply.github.com>
2022-10-26 20:03:39 +0300

    Update urls.py

--- Torquay/urls.py
+++ Torquay/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('visitors.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('visitors/', include('visitors.urls')),
+    path('', include('accounts.urls')),
     path('staff/', include('staff.urls')),
 ]
 
